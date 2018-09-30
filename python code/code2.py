bed347c620c68f8ad46436a47065ef07c207ca3f
surajv352 <42248852+surajv352@users.noreply.github.com>
2018-09-30 16:18:44 +0530

    Add files via upload

--- python code/code2.py
+++ python code/code2.py
@@ -1,20 +1,34 @@
-l = input()
-o = 0
-c = 0
-s = []
-s1 = ''
-for i in l:
-	if(i=='('):
-		o += 1
-		s.append(o)
+arr = list(map(int, input().split()))
+arr.sort()
+mi = ma = 0
+minimum = sum((mi + arr[j]) for j in range(0,len(arr)-1))
+maximum = sum((ma + arr[k]) for k in range(1,len(arr)))
+print(minimum,maximum)
+# for i in range(0,len(arr)-1):
+# 	mi = mi + arr[i]
+# for i in range(1,len(arr)):
+# 	ma = ma + arr[i]
 
-	elif(i==')'):
-		if(o==(c+2)):
-			s.append(o)
-		else:
-			c += 1
-			s.append(c)
-print(str(s))
-for i in s:
-	s1 += str(i)
-print(s1)
+
+
+
+#####################################################################
+#printing pattern
+
+'''n = int(input())
+k = 2*n - (n+1)
+print(k)
+for i in range(0, n):
+	for j in range(0, k):
+		print(end=" ")
+	k = k - 1
+	for j in range(0, i+1):
+          print("*", end="")
+	print("\r")'''
+
+#####################################################################
+ 
+'''for i in range(0, n): 
+    for j in range(0, i+1): 
+        print("* ",end="") 
+    print("\r")'''
--- python code/nltk1.py
+++ python code/nltk1.py
@@ -1,4 +1,40 @@
-from nltk.tokenize import sent_tokenize, word_tokenize
-text = 'he is a boy. he likes apple'
-print(sent_tokenize(text))
-print(word_tokenize(text))
+import nltk
+from nltk.corpus import state_union
+from nltk.tokenize import PunktSentenceTokenizer
+ 
+document = 'Our project is based on machine learning, it\'s implementation will be done by using python.'
+sentences = nltk.sent_tokenize(document)
+data = []   
+for sent in sentences:
+    data = data + nltk.pos_tag(nltk.word_tokenize(sent))
+for w in data:
+	if 'VB' in w[1]:
+		print(w)
+
+
+'''# from nltk.stem import PorterStemmer
+# from nltk.tokenize import word_tokenize
+# ps = PorterStemmer()
+# text = 'i love python language and i am a pythoner doing pythoning with python'
+# word = word_tokenize(text)
+# for w in word:
+# 	print(ps.stem(w))'''
+
+
+'''# from nltk.tokenize import word_tokenize
+# from nltk.corpus import stopwords
+# text = 'he is a boy and he likes mango juice but he hates brinjle.'
+# stop_words = set(stopwords.words('english'))
+# print(stop_words)
+# words = word_tokenize(text)
+# f_sent = []
+# for w in words:
+# 	if w not in stop_words:
+# 		f_sent.append(w)
+# print(f_sent)'''
+
+
+'''# from nltk.tokenize import sent_tokenize, word_tokenize
+# text = 'he is a boy. he likes apple'
+# print(sent_tokenize(text))
+# print(word_tokenize(text))'''
